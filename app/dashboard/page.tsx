ee1094b7c062c1eccf3b4891db38c721ff6a0ffa
Erickkipkoech <cheruiyotkipkoech612@gmail.com>
2024-06-12 22:13:58 +0300

    dashboard auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,36 @@
-export default function Page()
-{
-    return <h1>Hello Miot Dashboard</h1>
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import useSWR from "swr";
+import { fetchUser } from "../lib/auth";
+
+const fetcher =async () => {
+    try {
+        const user= await fetchUser();
+        return user;
+    } catch (error) {
+        throw error;
+    }
 }
+
+const Dashboard=()=>{
+    const router=useRouter();
+    const {data,error}=useSWR('',fetcher);
+
+    useEffect(()=>{
+        if(error){
+            router.push('/login');
+        }
+    },[error]);
+
+    if (error) return <div>Error:{error.message}</div>
+    if(!data) return <div>Loading...</div>
+
+    return (
+        <div>
+            <h1>Welcome to Miot, {data.username}</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
